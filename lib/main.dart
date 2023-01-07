fa0f17012318ba30431c1a38f74fc02926d9be00
M-amine-HNIOUA <mohamedaminehnioua@gmai.com>
2023-01-07 22:34:48 +0000

    ChatGPT-flutter-app v1.0.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:webview_flutter_plus/webview_flutter_plus.dart';
 
 void main() {
+  SystemChrome.setPreferredOrientations(
+    [
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ],
+  );
+  // Status bar Color:
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Color(0xff353740),
+      systemNavigationBarColor: Color(0xff353740),
+    ),
+  );
   runApp(
     const App(),
   );
@@ -24,16 +38,42 @@ class App extends StatelessWidget {
   }
 }
 
-class HomeWebView extends StatelessWidget {
+class HomeWebView extends StatefulWidget {
   const HomeWebView({super.key});
 
   @override
+  State<HomeWebView> createState() => _HomeWebViewState();
+}
+
+class _HomeWebViewState extends State<HomeWebView> {
+  bool _loading = true;
+  @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child: WebViewPlus(
-        initialUrl: 'https://chat.openai.com',
-        javascriptMode: JavascriptMode.unrestricted,
-        userAgent: 'random',
+    return SafeArea(
+      child: Stack(
+        alignment: AlignmentDirectional.center,
+        children: [
+          _loading
+              ? const CircularProgressIndicator(
+                  color: Colors.white,
+                  strokeWidth: 2.0,
+                )
+              : const Center(),
+          WebViewPlus(
+            initialUrl: 'https://chat.openai.com',
+            javascriptMode: JavascriptMode.unrestricted,
+            userAgent: 'random',
+            onPageStarted: (url) {
+              setState(() {
+                _loading = true;
+              });
+            },
+            onPageFinished: (url) {
+              _loading = false;
+            },
+            backgroundColor: const Color(0xff353740),
+          ),
+        ],
       ),
     );
   }
